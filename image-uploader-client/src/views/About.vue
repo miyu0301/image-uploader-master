54f23e954aa256ed36f7129e4fb4e54a28f6e0c8
youme <youme@mail>
2021-12-30 01:08:59 +0900

    almost completed

--- image-uploader-client/src/views/About.vue
+++ image-uploader-client/src/views/About.vue
@@ -1,13 +1,17 @@
 <template>
-  <!-- <div class="about">
-    <h1>This is an about page</h1>
-  </div> -->
-  <div>
-    <label>Uploaded Successfully!</label>
-    <div v-if="url">
-      <img :src="url">
-      <input v-model="url" />
-      <button @click="copyLink(url)">CopyLink</button>
+  <div class="body">
+    <div class="background_area">
+    <el-result icon="success" />
+      <p class="title_font">Uploaded Successfully!</p>
+      <!-- <div v-if="url"> -->
+        <img :src="url">
+        <div class="input_area">
+          <el-input v-model="url"/>
+          <el-row>
+            <el-button type="primary" @click="copyLink(url)">CopyLink</el-button>
+          </el-row>
+        </div>
+      <!-- </div> -->
     </div>
   </div>
 
@@ -15,12 +19,13 @@
 
 <script>
 export default {
-
-  name: "ImagePage",
   props: ['url'],
   data () {
     return {
     }
+  },
+  created: function () {
+    navigator.clipboard.writeText(this.url)
   },
   methods: {
     copyLink: function(url) {
@@ -30,4 +35,44 @@ export default {
 }
 
 </script>
+<style scoped>
+  .body {
+    height: 600px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .background_area {
+    height: 90%;
+    width: 50%;
+    border-width: 1px;
+    border-style: solid;
+    border-color: rgb(238, 238, 238);
+    border-image: initial;
+    border-radius: 15px;
+    box-shadow: rgba(0, 0, 0, 0.2) 0px 2px 12px 0px;
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+  }
+  .input_area {
+    display: flex;
+    width: 80%;
+    margin: 10px;
+  }
 
+  .title_font {
+    font-size: 30px;
+    font-weight: bold;
+    margin: 20px;
+  }
+  .el-result {
+    padding: 10px;
+  }
+  img {
+    height: 40%;
+    border-radius: 15px;
+    margin: 20px;
+  }
+</style>
--- image-uploader-client/src/views/Image.vue
+++ image-uploader-client/src/views/Image.vue
@@ -1,46 +1,35 @@
 <template id='app'>
     <el-row>
-    <div v-show="isLoading">
-      <label>Uploading...</label>
-      <template>
-        <div class="container">
-          <vue-loading type="spin" color="#333" :size="{ width: '50px', height: '50px' }"></vue-loading>
-        </div>
-      </template>
+    <div v-show="isLoading" class="loading_body">
+      <div class="background_loading_area">
+        <p>Uploading...</p>
+        <template>
+          <div class="container">
+            <vue-loading type="spin" color="#333" :size="{ width: '50px', height: '50px' }"></vue-loading>
+          </div>
+        </template>
+      </div>
     </div>
-    <div v-show="!isLoading">
-      <label>ファイルを選択
-          <input @change="changeFile" ref="file" type="file">
-      </label>
-      <el-row>
-          <el-col :span="24">
-              <el-card class="box-card">
-                  <div slot="header" class="clearfix">
-                      <span>test</span>
-                  </div>
-                  <el-table
-                          :data="images"
-                          style="width: 100%">
-                      <el-table-column
-                              prop="id"
-                              label="id"
-                              width="300"/>
-                      <el-table-column
-                              prop="image_name"
-                              label="image_name"
-                              width="300"/>
-                      <el-table-column
-                              prop="image_data"
-                              label="image_data"
-                              width="300"/>
-                      <el-table-column
-                              prop="ins_datetime"
-                              label="ins_datetime"
-                              width="300"/>
-                  </el-table>
-              </el-card>
-          </el-col>
-      </el-row>
+    <div v-show="!isLoading" class="upload_body">
+      <div class="background_upload_area">
+        <p class="title_font">Upload your image</p>
+        <p>File should be Jpeg, Png,…</p>
+        <div class="drop_area" @dragenter="dragEnter"
+                        @dragleave="dragLeave"
+                        @dragover.prevent
+                        @drop.prevent="dropFile"
+                        :class="{enter: isEnter}">
+          <img src="../image/image.svg">
+          <p>Drag & Drop your image here</p>
+        </div>
+        <p>Or</p>
+        <div>
+          <input type="file" ref="file" @change="changeFile" style="display:none;">
+          <el-row>
+            <el-button type="primary" @click="clickFileInput">Choose a file</el-button>
+          </el-row>
+        </div>
+      </div>
     </div>
   </el-row>
 </template>
@@ -58,6 +47,7 @@ export default {
   data () {
     return {
         images: [],
+        isEnter: false,
         isLoading: false,
         url: "",
     }
@@ -66,18 +56,31 @@ export default {
     await this.refresh()
   },
   methods: {
-    changeFile: async function (e) {
-
+    clickFileInput(){
+      this.$refs.file.click();
+    },
+    dragEnter(){
+      this.isEnter = true
+    },
+    dragLeave(){
+      this.isEnter = false
+    },
+    dropFile(){
+      this.isEnter = false
+      this.uploadFile(event.dataTransfer.files[0])
+    },
+    changeFile(e){
+      this.uploadFile(e.target.files[0])
+    },
+    uploadFile: async function(file){
       this.isLoading = true
       const _sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-      await _sleep(2000);
+      await _sleep(1000);
       
-      const files = e.target.files || e.dataTransfer.files
-
-      let file_name = files[0].name;
+      let file_name = file.name;
 
       let reader = new FileReader();
-      reader.readAsBinaryString(e.target.files[0]);
+      reader.readAsBinaryString(file);
       reader.onload = function(e){
         var send_data = {
             request: {
@@ -88,26 +91,14 @@ export default {
         send_data.name = file_name
         send_data.file = e.target.result
         axios.post('http://localhost:8080/', send_data)
-        // console.info('bbb');
-        // console.info(e.target);
-        // console.info(e.target.result);
       };
-      console.info('ccc')
 
-
-      // show image
-      this.url = URL.createObjectURL(files[0]);
-
+      this.url = URL.createObjectURL(file);
       this.$router.push({name : 'About', params: {url: this.url}})
-      console.info('ddd')
-
-
-      await this.refresh()
     },
     refresh: async function () {
       const res = await axios.get('http://localhost:8080/')
       this.images = res.data.images
-      // console.info(this.images)
     },
   }
 }
@@ -115,5 +106,58 @@ export default {
 </script>
 
 <style scoped>
-
+    .loading_body {
+      height: 300px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .upload_body {
+      height: 600px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .background_loading_area {
+      height: 40%;
+      width: 50%;
+      border-width: 1px;
+      border-style: solid;
+      border-color: rgb(238, 238, 238);
+      border-image: initial;
+      border-radius: 15px;
+      box-shadow: rgba(0, 0, 0, 0.2) 0px 2px 12px 0px;
+    }
+    .background_upload_area {
+      height: 90%;
+      width: 50%;
+      border-width: 1px;
+      border-style: solid;
+      border-color: rgb(238, 238, 238);
+      border-image: initial;
+      border-radius: 15px;
+      box-shadow: rgba(0, 0, 0, 0.2) 0px 2px 12px 0px;
+    }
+    .drop_area {
+        color: gray;
+        background-color: rgb(236, 245, 255);
+        font-weight: bold;
+        font-size: 1.2em;
+        display: flex;
+        flex-flow: column;
+        justify-content: center;
+        align-items: center;
+        width: 60%;
+        height: 50%;
+        border: 5px solid rgb(140, 197, 255);
+        border-radius: 15px;
+        margin: 0 auto;
+    }
+    .enter {
+    border: 10px dotted rgb(140, 197, 255);
+    }
+    .title_font {
+      font-size: 30px;
+      font-weight: bold;
+    }
 </style>;
